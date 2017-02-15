d0683f1736e4422b35ba6d21e02331d7aede8fc9
mech <mech@me.com>
2017-02-15 18:50:34 +0800

    Begin to use styled-components

--- app/AuthRoute.jsx
+++ app/AuthRoute.jsx
@@ -3,6 +3,8 @@ import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import auth from './auth'
 
+// From the props, take out "component" and assigned to "Component" variable, then
+// store the rest of the props into "restOfProps"
 const AuthRoute = ({ component: Component, ...restOfProps }) => {
   console.log(restOfProps);
   return <Route
